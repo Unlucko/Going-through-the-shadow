c1aaeca99792d562049d3aa02e8a0ac0e0eb4bd6
luciaardila <82615427+luciaardila@users.noreply.github.com>
2021-05-23 18:25:57 -0500

    Update menu_INICIA TODO.py

--- menu_INICIA TODO.py
+++ menu_INICIA TODO.py
@@ -4,6 +4,19 @@ import pygame, random, help, credits, juego
 
 def init ():
 
+    pygame.init()
+    screen_size = (1500, 1000)
+    yellow         = [255, 255, 0]
+    black       = (0, 0, 0)
+    white          = (255, 255, 255)
+    clock       = pygame.time.Clock()
+
+    screen      = pygame.display.set_mode(screen_size)
+
+
+    pygame.mixer.music.load("fondo_sound.wav")
+    pygame.mixer.music.play(2)
+
 
     # estrellas
     coor_list = []
@@ -30,7 +43,7 @@ def init ():
             if len(clr) == 4:
                 self.surf.set_alpha(clr[3])
 
-
+            #atributos texto
             self.font = pygame.font.SysFont(font, font_size)
             self.txt = text
             self.font_clr = font_clr
@@ -50,29 +63,17 @@ def init ():
             if self.rect.collidepoint(pos):
                 self.curclr = self.cngclr
 
+
+
         def call_back(self, *args):
             if self.func:
                 return self.func(*args)
 
-    class text:
-        def __init__(self, msg, position, clr=[100, 100, 100], font="Segoe Print", font_size=15, mid=False):
-            self.position = position
-            self.font = pygame.font.SysFont(font, font_size)
-            self.txt_surf = self.font.render(msg, 1, clr)
-
-            if len(clr) == 4:
-                self.txt_surf.set_alpha(clr[3])
-
-            if mid:
-                self.position = self.txt_surf.get_rect(center=position)
-
-
-        def draw(self, screen):
-            screen.blit(self.txt_surf, self.position)
-
 
     # call back funciones
     def fn1():
+        pygame.mixer.music.load("click.wav")
+        pygame.mixer.music.play()
         juego.init() #abre la ventana del juego
         pygame.quit()
     def fn2():
@@ -81,64 +82,56 @@ def init ():
     def fn3():
         pygame.quit() #hace que se cierre la ventana
     def fn4():
-        print ("welcome to the game")
-    def fn5():
         credits.init()
         pygame.quit()
 
 
+    #todos los botones necesarios
+
+    button1 = button(position=(750, 800/2), size=(140, 45), clr=white, cngclr=yellow, func=fn1, text='Play', font = "segoeprint", font_size=35, font_clr = black)
+    button2 = button((750, 1050/2), (140, 45), white, yellow, fn2, 'Help', font = "segoeprint", font_size=35, font_clr = black)
+    button3 = button((750, 1300 / 2), (140, 45), white, yellow, fn4, 'Credits', font="segoeprint", font_size=35,font_clr=black)
+    button4 = button((750, 1550/2), (140, 45), white, yellow, fn3, 'Exit', font = "segoeprint", font_size=35, font_clr = black)
 
 
+    button_list = [button1, button2, button4, button3]
+
+    Title = button((750, 160), (1500, 80), black, black, fn4, 'Going thought the shadow', font="sourcecodeproblack",font_size=72, font_clr=yellow)
+    mifuente = pygame.font.SysFont("sourcecodeproblack", 72)
+    titulo = mifuente.render("Going thought the shadow", 0, yellow)
 
 
-    pygame.init()
-    screen_size = (1500, 1000)
-    size        = 10
-    clr         = [255, 255, 0]
-    black       = (0, 0, 0)
-    white          = (255, 255, 255)
-    font_size   = 15
-    font        = pygame.font.Font(None, font_size)
-    clock       = pygame.time.Clock()
-
-    screen      = pygame.display.set_mode(screen_size)
-
-    pygame.mixer.music.load("fondo_sound.wav")
-    pygame.mixer.music.play(2)
-
-    button1 = button(position=(750, 800/2), size=(140, 45), clr=(255, 255, 255), cngclr=(255, 255, 0), func=fn1, text='Play', font = "segoeprint", font_size=35, font_clr = (0, 0, 0))
-    button2 = button((750, 1050/2), (140, 45), (255, 255, 255), (255, 255, 0), fn2, 'Help', font = "segoeprint", font_size=35, font_clr = (0, 0, 0))
-    button3 = button((750, 1300 / 2), (140, 45), (255, 255, 255), (255, 255, 0), fn5, 'Credits', font="segoeprint", font_size=35,font_clr=(0, 0, 0))
-    button4 = button((750, 1550/2), (140, 45), (255, 255, 255), (255, 255, 0), fn3, 'Exit', font = "segoeprint", font_size=35, font_clr = (0, 0, 0))
-    Title = button((750, 160), (1500, 80), (0, 0, 0), (0, 0, 0), fn4, 'Going thought the shadow', font = "sourcecodeproblack", font_size=72, font_clr = (255, 255, 0))
-
-    button_list = [button1, button2, button4, button3, Title]
+    sound = pygame.mixer.Sound("click.wav")
 
 
-
-    crash = True
-    while crash:
+    game_over = True
+    while game_over:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                crash = False
+                game_over = False
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    crash = False
+                    game_over = False
 
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     pos = pygame.mouse.get_pos()
                     for b in button_list:
                         if b.rect.collidepoint(pos):
+                            sound.play()
                             b.call_back()
+                            pygame.quit()
+
+
 
 
         screen.fill(black)
 
+
         for b in button_list:
             b.draw(screen)
 
-        # animación estrallas
+        # animación estrellas
         for coord in coor_list:
             x_e = coord[0]
             y_e = coord[1]
@@ -146,6 +139,8 @@ def init ():
             coord[1] += 1
             if coord[1] > 1000:
                 coord[1] = 0
+
+        screen.blit (titulo, (250, 160))
 
         pygame.display.flip()
         clock.tick(60)
